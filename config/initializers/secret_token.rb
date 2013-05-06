a0d0aa4c0edd5a6f63333dda4b625bc6872a9fe4
Chris Mailloux <chrismayu@gmail.com>
2013-05-05 22:06:32 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FusionApp::Application.config.secret_token = 'f6b5200b9ad6086d9abfa6cfc8b9394391ce453cf0d5dd415347d0fae0dd9d3a8cb12bee4142d67d97d9e48bb0612eb340ef97647f10df3b6061e53f5dd65312'
